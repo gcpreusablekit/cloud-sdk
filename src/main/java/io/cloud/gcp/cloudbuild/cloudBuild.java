230c86849bed20aa4d66a25f231679b703741c9d
gcpuser <qeasandbox21@gmail.com>
2021-12-14 12:31:42 +0530

    inital commit

---
+++ src/main/java/io/cloud/gcp/cloudbuild/cloudBuild.java
@@ -0,0 +1,4 @@
+package io.cloud.gcp.cloudbuild;
+
+public class cloudBuild {
+}
---
+++ src/main/java/io/cloud/gcp/compute/computeEngine.java
@@ -0,0 +1,12 @@
+package io.cloud.gcp.compute;
+
+import com.google.api.client.googleapis.compute.ComputeCredential;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class computeEngine {
+
+    private static ComputeCredential instance=null;
+}
