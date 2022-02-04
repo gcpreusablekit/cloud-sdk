196796c4f6924d7ec78e2c680fb79a96d07a2e8f
Aiswarya Ayyappan <aiswaryaayyappan99@gmail.com>
2022-02-03 23:01:59 -0800

    update

--- src/main/java/io/cloud/gcp/Cloudstorage/storage.java
+++ src/main/java/io/cloud/gcp/Cloudstorage/storage.java
@@ -8,7 +8,22 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class storage {
+    public static void main(String[] args) {
 
+        String projectId = "qea-sandbox";
+        String bucketName = "qea-reusablekit1";
+        String objectName = "kitten";
+        String filePath = "C:/Users/ASWYAYPFST/Downloads/images/Kitten.png" ;
+        storage s=new storage();
+        s.createBucket(projectId,bucketName);
+       s.getBucketMetadata(projectId,bucketName);
+        s.listBuckets(projectId);
+        s.uploadObject( projectId, bucketName, objectName, filePath);
+        s.makeObjectPublic(projectId,bucketName,objectName);
+        s.listObjects(projectId,bucketName);
+       s.deleteObject(projectId,bucketName,objectName);
+       s.deleteBucket(projectId,bucketName);
+    }
     public BucketInfo createBucket(String projectId, String bucketName) {
 
         Bucket bucket = null;
@@ -38,7 +53,7 @@ public class storage {
         return buckets;
     }
     public BlobId uploadObject(
-            String projectId, String bucketName, String objectName, String filePath) throws IOException {
+            String projectId, String bucketName, String objectName, String filePath) {
 
         BlobId blobId = null;
         try {
